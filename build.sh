65c9c7f3f33c446ccd22e9e41acd657f47c7da3e
yuanlin <im.yuanlinlin@gmail.com>
2021-11-21 00:30:43 +0800

    fixed build script copy js error

--- build.sh
+++ build.sh
@@ -8,9 +8,8 @@ awk '{sub("node_modules/gl-matrix","./js")}1' dist/index.html.tmp > dist/index.h
 rm dist/index.html dist/index.html.tmp
 mv dist/index.html.tmp2 dist/index.html
 
-mkdir dist/js
+cp -r js dist/js/
 cp node_modules/webgl-obj-loader/dist/webgl-obj-loader.min.js dist/js/webgl-obj-loader.min.js
 cp node_modules/gl-matrix/gl-matrix-min.js dist/js/gl-matrix-min.js
-cp js/*.js dist/js/
 
 cp -r assets dist
