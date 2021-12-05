0fa83ef50643bff0e425cf824d932c095c6460cc
yuanlin <im.yuanlinlin@gmail.com>
2021-12-05 21:08:12 +0800

    update comments in main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,28 +12,31 @@ function main() {
 
   const { player, inputController } = app;
 
-  if (inputController)
-    inputController.onMouseMove = (x, y) => {
-      player.setRotation({ w: player.rotationRadius + x * .001 })
-    }
-
   player.onNextTick = (deltaTime) => {
 
     if (!inputController) return;
+
+    // When mouse move, rotate the camera
+    inputController.onMouseMove = (x, y) => {
+      // x, y means the move distance of mouse
+      // use these params to calculate new rotation of camera!
+      player.setRotation({ w: player.rotationRadius + x * .001 })
+      console.log('Mouse move! delta X is ', x, ', delta Y is', y)
+    }
 
     // When press "W" key, player move forward
     if (inputController.isKeyPressed('w'))
       player.setPosition({ z: player.positionZ + (deltaTime * player.moveSpeed) })
 
-    // When press "S" key, player move forward
+    // When press "S" key, player move backward 
     if (inputController.isKeyPressed('s'))
       player.setPosition({ z: player.positionZ - (deltaTime * player.moveSpeed) })
 
-    // When press "A" key, player move forward
+    // When press "A" key, player move left 
     if (inputController.isKeyPressed('a'))
       player.setPosition({ x: player.positionX + (deltaTime * player.moveSpeed) })
 
-    // When press "D" key, player move forward
+    // When press "D" key, player move right 
     if (inputController.isKeyPressed('d'))
       player.setPosition({ x: player.positionX - (deltaTime * player.moveSpeed) })
   }
